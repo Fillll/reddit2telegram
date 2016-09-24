56df9892bea0ed3d625bd547830a84ecd15f9c2a
Ngalim Siregar <ngalim.siregar@gmail.com>
2016-09-25 01:10:17 +0900

    modify get url for gfycat as get_gif

--- channels/r_asiangirlsbeingcute/app.py
+++ channels/r_asiangirlsbeingcute/app.py
@@ -2,15 +2,29 @@
 
 import os
 
-from utils import get_url, download_file, telegram_autoplay_limit
-
+from utils import download_file, telegram_autoplay_limit
+from urllib.parse import urlparse
 
 subreddit = 'asiangirlsbeingcute'
 t_channel = '@asiangirlsbeingcute'
 
+def get_gif(submission):
+    url = submission.url
+    # TODO: Better url validation
+    if url.endswith('.gif'):
+        return 'gif', url
+    elif url.endswith('.gifv'):
+        return 'gif', url[0:-1]
+    elif urlparse(url).netloc == 'www.reddit.com':
+        return 'text', None
+    elif urlparse(url).netloc == 'gfycat.com':
+        gifurl = 'https://thumbs.gfycat.com' + urlparse(url).path + '-small.gif'
+        return 'gif', gifurl
+    else:
+        return 'other', url
 
 def send_post(submission, bot):
-    what, gif_url = get_url(submission)
+    what, gif_url = get_gif(submission)
     if what != 'gif':
         return False
     # Download gif
