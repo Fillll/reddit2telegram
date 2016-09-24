0bad0d5395a7cfd807bff4d280fe8112de9186ac
Alexander F <git@fillll.ru>
2016-09-24 11:08:14 -0400

    50 MB telegram limit.

--- utils.py
+++ utils.py
@@ -24,9 +24,14 @@ def download_file(url, filename):
     # http://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True)
+    chunk_counter = 0
     with open(filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
                 #f.flush() commented by recommendation from J.F.Sebastian
+                chunk_counter += 1
+                # It is not possible to send greater than 50 MB via Telegram
+                if chunk_counter > 50 * 1024:
+                    return False
     return True
