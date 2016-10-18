5e65eeaee7e70d0eb5d2d14acb8d7e783b2626de
Alexander F <git@fillll.ru>
2016-10-18 12:03:39 -0400

    Workaround for strange fails in @r_unexpected.

--- channels/r_unexpected/app.py
+++ channels/r_unexpected/app.py
@@ -1,19 +1,26 @@
 # encoding: utf-8
 
-from utils import get_url
 import logging
+
 from pytgbot.bot import Bot
 from pytgbot.exceptions import TgApiException
 
+from utils import get_url
+
+
 logger = logging.getLogger(__name__)
+
 
 subreddit = 'unexpected'
 t_channel = '@r_unexpected'
 
-NSFW_emoji = u"\U0001F51E"
+
+NSFW_emoji = u'\U0001F51E'
+
 
 def send_post(submission, bot):
-    bot = Bot(bot._token)
+    bot_old = bot
+    bot = Bot(bot_old._token)
     what, gif_url, _ = get_url(submission)
     if what != 'gif':
         return False
@@ -34,5 +41,17 @@ def send_post(submission, bot):
         bot.send_document(t_channel, document=gif_url, caption=text)
     except TgApiException:
         from pytgbot.api_types.sendable.files import InputFileFromURL
-        bot.send_document(t_channel, document=InputFileFromURL(gif_url), caption=text)
+        try:
+            bot.send_document(t_channel, document=InputFileFromURL(gif_url), caption=text)
+        except:
+            import os
+            from utils import download_file, telegram_autoplay_limit
+            filename = 'r_unexpected.gif'
+            if not download_file(gif_url, filename):
+                return False
+            if os.path.getsize(filename) > telegram_autoplay_limit:
+                return False
+            f = open(filename, 'rb')
+            bot_old.sendDocument(t_channel, f, caption=text)
+            f.close()
     return True
