9c959ac61ca0fd4465fd8000fb2fbf0b4c8b5dfb
Alexander F <git@fillll.ru>
2016-11-13 10:13:47 -0500

    History subreddits updated.

--- channels/r_history/app.py
+++ channels/r_history/app.py
@@ -2,6 +2,7 @@
 
 import os
 import random
+from urllib.parse import urlparse
 
 from utils import (get_url, download_file, telegram_autoplay_limit,
                    just_send_an_album)
@@ -17,36 +18,36 @@ def weighted_random(d):
 
 
 def define_channel_for_today():
-    channels = {'HistoryPorn': 8,
-        'ArchivePorn': 0.5,
+    channels = {'HistoryPorn': 0.5,
+        'ArchivePorn': 0.01,
         # Add something from this list:
         # /r/ADifferentEra
-        # /r/BattlePaintings
-        # /r/BiographyFilms
-        # /r/Castles
-        # /r/Colorization
-        # /r/ColorizedHistory
+        'BattlePaintings': 0.02,
+        'BiographyFilms': 0.005,
+        'Castles': 0.04,
+        'Colorization': 0.1,
+        'ColorizedHistory': 0.02,
         # /r/CombatFootage
-        # /r/FortPorn
+        'FortPorn': 0.01,
         # /r/History
         # /r/HistoryNetwork
-        # /r/ImagesOfHistory
-        # /r/ImaginaryHistory
-        # /r/ImaginaryPolitics
+        'ImagesOfHistory': 0.005,
+        'ImaginaryHistory': 0.005,
+        'ImaginaryPolitics': 0.005,
         # /r/MapIt
-        # /r/MegalithPorn
-        # /r/OldIndia
-        # /r/OldSchoolCool
-        # /r/OldSchoolCreepy
-        # /r/Presidents
-        'PropagandaPosters': 1,
-        # /r/RedditThroughHistory
+        'MegalithPorn': 0.005,
+        'OldIndia': 0.005,
+        'OldSchoolCool': 0.15,
+        'OldSchoolCreepy': 0.005,
+        'Presidents': 0.005,
+        'PropagandaPosters': 0.1,
+        'RedditThroughHistory': 0.005,
         # /r/RestofHistoryPorn
-        # /r/TheWayWeWere
-        # /r/WarshipPorn
-        # /r/WWIIPics
+        'TheWayWeWere': 0.01,
+        'WarshipPorn': 0.02,
+        'WWIIPics': 0.02,
         # /r/WWIIPlanes
-        # /r/ColdWarPosters 
+        'ColdWarPosters': 0.005
     }
     return weighted_random(channels)
 
@@ -73,35 +74,36 @@ def send_post(submission, bot):
     title = submission.title
     link = submission.short_link
     text = '{}\n{}'.format(title, link)
+    domain = urlparse(url).netloc
 
     if what == 'text':
         return False
-
     elif what == 'album':
         just_send_message(submission, bot)
         just_send_an_album(t_channel, url, bot)
         return True
-
     elif what == 'other':
-        return False
+        if domain in ('www.youtube.com', 'youtu.be'):
+            text = '{}\n{}\n\n{}'.format(title, url, link)
+            bot.sendMessage(t_channel, text)
+        else:
+            return False
 
     filename = 'r_history.{}'.format(ext)
     if not download_file(url, filename):
-        return just_send_message(submission, bot)
+        return False
     if os.path.getsize(filename) > telegram_autoplay_limit:
-        return just_send_message(submission, bot)
+        return False
 
     if what == 'gif':
         f = open(filename, 'rb')
         bot.sendDocument(t_channel, f, caption=text)
         f.close()
         return True
-
     elif what == 'img':
         f = open(filename, 'rb')
         bot.sendPhoto(t_channel, f, caption=text)
         f.close()
         return True
-
     else:
         return False
