969bf153297e128e004ac7755605343e804016b5
Alexander F <git@fillll.ru>
2016-11-19 06:47:59 -0500

    Minor improvements in @r_jokes.

--- channels/r_jokes/app.py
+++ channels/r_jokes/app.py
@@ -1,5 +1,8 @@
 #encoding:utf-8
 
+# Some utils can be useful.
+from utils import get_url
+
 
 # Write here subreddit name. Like this one for /r/jokes.
 subreddit = 'jokes'
@@ -8,12 +11,21 @@ t_channel = '@r_jokes'
 
 
 def send_post(submission, r2t):
+    # Check what is inside this submission.
+    what, _, _ = get_url(submission)
+
+    # If inside is something but not text
+    # then we do not need this submission.
+    if what != 'text':
+        False
+
     # To read more about dealing with reddit submission please
     # visit https://praw.readthedocs.io/.
     title = submission.title
     punchline = submission.selftext
     link = submission.short_link
-    text = '{}\n\n{}\n\n{}\n{}'.format(title, punchline, link, t_channel)
+    text = '{title}\n\n{body}\n\n{link}\n{channel}'.format(
+            title=title, body=punchline, link=link, channel=t_channel)
 
     # To read more about sending massages to telegram please
     # visit https://github.com/nickoala/telepot/tree/master/examples/simple
