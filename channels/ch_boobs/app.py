c2ce7113e6c8e1babf7b323961ca83e59708d743
Alexander F <git@fillll.ru>
2016-12-16 14:59:21 -0500

    More boobs.

--- channels/ch_boobs/app.py
+++ channels/ch_boobs/app.py
@@ -2,11 +2,18 @@
 
 import time
 
-from utils import get_url
+from utils import get_url, weighted_random_subreddit
 
 
-subreddit = 'boobs'
-t_channel = '-1001052042617'
+main_channel = '-1001052042617'
+fake_channel = '@r_channels_test'
+
+
+t_channel = weighted_random_subreddit({
+    main_channel: 0.05,
+    fake_channel: 0.095
+})
+subreddit = 'boobs' if t_channel == main_channel else 'all'
 
 
 def send_post(submission, r2t):
@@ -15,17 +22,38 @@ def send_post(submission, r2t):
     link = submission.short_link
     text = '{}\n{}'.format(title, link)
 
-    if what in ('gif', 'img'):
-        r2t.send_text('🔞🔞🔞🔞🔞🔞')
-        time.sleep(10)
-        success = r2t.send_gif_img(what, url, ext, text)
-        if success is False:
-            return False
-        for i in range(4):
-            time.sleep(3.14159 / 2.718281828)
+    if t_channel == 'boobs':
+        if what in ('gif', 'img'):
             r2t.send_text('🔞🔞🔞🔞🔞🔞')
-            time.sleep(3.14159 / 2.718281828)
-            r2t.send_text('👆👆👆👆👆👆')
-        return True    
+            time.sleep(10)
+            success = r2t.send_gif_img(what, url, ext, text)
+            if success is False:
+                return False
+            for i in range(4):
+                time.sleep(3.14159 / 2.718281828)
+                r2t.send_text('🔞🔞🔞🔞🔞🔞')
+                time.sleep(3.14159 / 2.718281828)
+                r2t.send_text('👆👆👆👆👆👆')
+            return True    
+        else:
+            return False
     else:
-        return False
+        text = '{}\n\n/r/{}\n{}'.format(title, submission.subreddit, link)
+        if what == 'text':
+            punchline = submission.selftext
+            text = '{}\n\n{}\n\n/r/{}\n{}'.format(title, punchline, subreddit, link)
+            return r2t.send_text(text)
+        elif what == 'other':
+            base_url = submission.url
+            text = '{}\n{}\n\n/r/{}\n{}'.format(title, base_url, subreddit, link)
+            return r2t.send_text(text)
+        elif what == 'album':
+            base_url = submission.url
+            text = '{}\n{}\n\n/r/{}\n{}'.format(title, base_url, subreddit, link)
+            r2t.send_text(text)
+            r2t.send_album(url)
+            return True
+        elif what in ('gif', 'img'):
+            return r2t.send_gif_img(what, url, ext, text)
+        else:
+            return False
