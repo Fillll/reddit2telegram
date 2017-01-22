d57bdeddcb1f7aee8fe7f091764e61cb10e6ff68
Alexander F <git@fillll.ru>
2017-01-22 11:40:58 +0000

    Metro related for @ya_metro (Moscow Metro).

---
+++ channels/ya_metro/app.py
@@ -0,0 +1,39 @@
+#encoding:utf-8
+
+from urllib.parse import urlparse
+
+from utils import get_url
+
+
+t_channel = '@ya_metro'
+subreddit = 'Subways'
+
+
+def send_post(submission, r2t):
+    what, url, ext = get_url(submission)
+    title = submission.title
+    link = submission.shortlink
+    text = '{}\n{}'.format(title, link)
+
+    if what == 'text':
+        return False
+    elif what == 'album':
+        base_url = submission.url
+        text = '{}\n{}\n\n{}'.format(title, base_url, link)
+        r2t.send_text(text)
+        r2t.send_album(url)
+        return True
+    elif what == 'other':
+        domain = urlparse(url).netloc
+        if domain in ('www.youtube.com', 'youtu.be'):
+            text = '{}\n{}\n\n{}'.format(title, url, link)
+            return r2t.send_text(text)
+        elif submission.score > 10:
+            text = '{}\n{}\n\n{}'.format(title, url, link)
+            return r2t.send_text(text)
+        else:
+            return False
+    elif what in ('gif', 'img'):
+        return r2t.send_gif_img(what, url, ext, text)
+    else:
+        return False
