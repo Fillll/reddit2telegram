6af26c1ea2ca9a840f2cf47c9fe49e3116d707c6
Fillll <Fillll@users.noreply.github.com>
2017-08-01 07:37:59 +0300

    Do not send albums for ninawa

--- channels/p_ninawa/app.py
+++ channels/p_ninawa/app.py
@@ -47,7 +47,7 @@ def send_post(submission, r2t):
         base_url = submission.url
         text = '{}\n{}\n\n/r/{}\n{}'.format(title, base_url, subreddit, link)
         r2t.send_text(text)
-        r2t.send_album(url)
+        #r2t.send_album(url)
         return True
     elif what in ('gif', 'img'):
         return r2t.send_gif_img(what, url, ext, text)
