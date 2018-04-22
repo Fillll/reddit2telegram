1fc2769b6d1be316746be817c54513b60d7e2d33
Alexander F <git@fillll.ru>
2018-04-22 06:59:31 +0000

    @pythondaily to old format.

--- channels/pythondaily/app.py
+++ channels/pythondaily/app.py
@@ -17,4 +17,10 @@ subreddit = weighted_random_subreddit({
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission)
+    return r2t.send_simple(submission,
+        text='{title}\n\n{self_text}\n\n/r/{subreddit_name}\n{short_link}',
+        gif='{title}\n\n/r/{subreddit_name}\n{short_link}',
+        img='{title}\n\n/r/{subreddit_name}\n{short_link}',
+        album='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}',
+        other='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}'
+    )
