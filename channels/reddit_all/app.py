5596af95b532fecbdc815782958b5842ca9d153a
Alexander F <git@fillll.ru>
2018-05-03 16:44:19 +0000

    Min upvotes for @reddit_all is 12345.

--- channels/reddit_all/app.py
+++ channels/reddit_all/app.py
@@ -1,19 +1,12 @@
 #encoding:utf-8
 
-from utils import get_url
-from utils import SupplyResult
-
-
 t_channel = '@reddit_all'
 subreddit = 'all'
 
 
 def send_post(submission, r2t):
-    upvotes = submission.score
-    if upvotes < 10000:
-        return SupplyResult.SKIP_FOR_NOW
-
     return r2t.send_simple(submission,
+        upvotes_limit=12345,
         text='{title}\n\n{self_text}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
         other='{title}\n{link}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
         album='{title}\n{link}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
