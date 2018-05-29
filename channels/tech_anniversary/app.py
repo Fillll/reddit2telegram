f0f4d89012e6860ab98553eb7395222335188bb1
Alexander F <git@fillll.ru>
2018-05-29 09:06:09 +0000

    Proper sleep before party.

--- channels/tech_anniversary/app.py
+++ channels/tech_anniversary/app.py
@@ -1,6 +1,6 @@
 #encoding:utf-8
 
-import random
+import time
 
 from utils import SupplyResult
 from utils.tech import is_birthday_today, get_all_public_channels
@@ -16,6 +16,7 @@ def send_post(submission, r2t):
     for channel in channels_list:
         bd_party, years = is_birthday_today(r2t, channel)
         if bd_party:
+            time.sleep(10)
             r2t.send_text('{channel} is {years} old.'.format(channel=channel, years=years))
     # It's not a proper supply, so just stop.
     return SupplyResult.STOP_THIS_SUPPLY
