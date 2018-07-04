fe34437a76bfc2c6b15d24a2f71b4941c9875872
Tognee <alexio64bit@gmail.com>
2018-07-04 15:51:49 +0200

    Revert "Enhancements for FakeAlbumCovers"
    This reverts commit 28d38d94ec471f072fa0ff19fbc8c0d7eb228254.

--- channels/fakealbumcovers/app.py
+++ channels/fakealbumcovers/app.py
@@ -10,7 +10,7 @@ subreddit = 'fakealbumcovers'
 # This is for your public telegram channel.
 t_channel = '@fakealbumcovers'
 
-separators = [" - ", " — ", " – ", " -- ", " | ", " • ", "- ", " // ", "-", " by "]
+separators = [" - ", " — ", " | ", "- ", "-", " by "]
 
 def send_post(submission, r2t):
     what, url, ext = get_url(submission)
@@ -18,41 +18,25 @@ def send_post(submission, r2t):
     fullTitle = submission.title
     link = submission.shortlink
     
-    if fullTitle.lower().startswith("[request]") or submission.link_flair_text.strip().lower() == 'request':
+    if fullTitle.lower().startswith("[request]"):
         return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
     
     title = fullTitle
     artist = ""
+    
     for sep in separators:
         if sep in fullTitle:
             if sep == " by ":
                 artist = fullTitle[fullTitle.find(sep)+len(sep):]
                 title = fullTitle[:fullTitle.find(sep)]
-            elif sep == "-":
-                if title.lower().endswith("(self-titled)"):
-                    artist = fullTitle[:-len("(self-titled)")]
-                    title = artist
             else:
                 artist = fullTitle[:fullTitle.find(sep)]
                 title = fullTitle[fullTitle.find(sep)+len(sep):]
             break
-
-    if (title.lower() == "self titled" or title[1:-1].lower() == "self titled" or title.lower() == "self-titled" or title[1:-1].lower() == "self-titled") and artist != "":
+     
+    if title.lower() == "self titled":
         title = artist
-
-    if "self titled" in title.lower():
-        if artist != "":
-            title = artist
-        else:
-            if title[title.lower.find("self titled")-1].lower() in ["(","["]:
-                title = title[:title.lower.find("self titled")-1]
-            else:
-                title = title[:title.lower.find("self titled")]
-            artist = title
-
-    if title.startswith('"') and title.endswith('"'):
-        title = title[1:-1]
-
+    
     if artist != "":
         text = '<b>{title}</b>\n<i>{artist}</i>\n\n▶️ {link}\n🎵 {channel}'.format(
                 title=title, artist=artist, link=link, channel=t_channel)
@@ -61,7 +45,9 @@ def send_post(submission, r2t):
                 title=title, link=link, channel=t_channel)
     
     
-    if what == 'img':
+    if what in ['text', 'other', 'album']:
+        return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
+    elif what == 'img':
         if r2t.dup_check_and_mark(url) is True:
             return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
         return r2t.send_gif_img(what, url, ext, text, parse_mode='HTML')
