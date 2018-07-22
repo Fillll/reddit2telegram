924425925616eee4de832d3fee3d34c9d5c6a20a
Ngalim Siregar <ngalim.siregar@gmail.com>
2018-07-22 18:09:44 +0900

    split words to 200

--- channels/pythondaily/app.py
+++ channels/pythondaily/app.py
@@ -17,8 +17,11 @@ subreddit = weighted_random_subreddit({
 
 
 def send_post(submission, r2t):
+    info = submission.selftext
+    lwords = info.split(' ')[:200]
+    words = ' '.join(lwords)
     return r2t.send_simple(submission,
-        text='{title}\n\n{self_text}\n\n/r/{subreddit_name}\n{short_link}',
+        text='{title}\n\n' + words +'\n\n/r/{subreddit_name}\n{short_link}',
         gif='{title}\n\n/r/{subreddit_name}\n{short_link}',
         img='{title}\n\n/r/{subreddit_name}\n{short_link}',
         album='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}',
