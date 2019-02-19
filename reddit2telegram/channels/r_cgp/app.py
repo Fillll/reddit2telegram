4f14107f170fdc14e7ed56db8534d56796ca1cc0
Alexander F <git@fillll.ru>
2019-02-19 17:56:42 +0000

    @r_CoolGithubProjects

---
+++ reddit2telegram/channels/r_cgp/app.py
@@ -0,0 +1,13 @@
+#encoding:utf-8
+
+from utils import weighted_random_subreddit
+
+
+subreddit = weighted_random_subreddit({
+    'CoolGithubProjects': 1
+})
+t_channel = '@r_CoolGithubProjects'
+
+
+def send_post(submission, r2t):
+    return r2t.send_simple(submission)
