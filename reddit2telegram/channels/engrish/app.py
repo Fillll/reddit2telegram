f8d4bed053e0ae58b758bc88916e1f83728400e1
Samuel <samuelcarballogarcia8@gmail.com>
2019-03-01 19:36:43 +0100

    Create app.py

---
+++ reddit2telegram/channels/engrish/app.py
@@ -0,0 +1,38 @@
+#encoding:utf-8
+
+from utils import get_url
+from utils import SupplyResult
+
+
+subreddit = 'engrish'
+t_channel = '@r_engrish'
+
+
+def send_post(submission, r2t):
+    what, url, ext = get_url(submission)
+    if what not in ('gif', 'img'):
+        return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
+
+    title = submission.title
+    link = submission.shortlink
+    fair = submission.link_flair_text
+    fair_text = ''
+    if fair is not None:
+        fair_text = '[{f}] '.format(f=fair)
+
+    if submission.over_18:
+        url = submission.url
+        text = '🔞NSFW🔞\n{u}\n{f}{t}\n\n{l}\n\nby {c}'.format(
+            u=url,
+            f=fair_text,
+            t=title,
+            l=link,
+            c=t_channel)
+        return r2t.send_text(text, disable_web_page_preview=True)
+
+    text = '{f}{t}\n{l}\n\nby {c}'.format(
+        f=fair_text,
+        t=title,
+        l=link,
+        c=t_channel)
+    return r2t.send_gif_img(what, url, ext, text)
