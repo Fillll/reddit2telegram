7a3673b54a7dbc3d228f26579de932595d9196e6
Alexander F <git@fillll.ru>
2019-03-16 14:42:50 +0000

    @r_LiverpoolFC

--- reddit2telegram/channels/r_liverpoolfc/app.py
+++ reddit2telegram/channels/r_liverpoolfc/app.py
@@ -5,4 +5,4 @@ t_channel = '@r_LiverpoolFC'
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission, 100, True)
+    return r2t.send_simple(submission, min_upvotes_limit=100, check_dups=True)
