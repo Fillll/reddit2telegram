7be0938ba217d968c8d21f312ff9bf2864429933
Alexander F <git@fillll.ru>
2019-07-28 07:45:49 +0000

    New format for r_gamingmemes.

--- reddit2telegram/channels/r_gamingmemes/app.py
+++ reddit2telegram/channels/r_gamingmemes/app.py
@@ -5,4 +5,10 @@ t_channel = '@r_gamingmemes'
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission)
+    return r2t.send_simple(submission,
+        text='{title}\n\n{self_text}\n\n/r/{subreddit_name}\n{channel}',
+        other='{title}\n{link}\n\n/r/{subreddit_name}\n{channel}',
+        album='{title}\n{link}\n\n/r/{subreddit_name}\n{channel}',
+        gif='{title}\n\n/r/{subreddit_name}\n{channel}',
+        img='{title}\n\n/r/{subreddit_name}\n{channel}'
+    )
