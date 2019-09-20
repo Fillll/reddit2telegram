8f5b1bc1a984d1fcc060e945a9e9a57466653694
Sima-ua <55588145+Sima-ua@users.noreply.github.com>
2019-09-20 15:49:02 +0300

    Update app.py

--- reddit2telegram/channels/r_funny/app.py
+++ reddit2telegram/channels/r_funny/app.py
@@ -5,13 +5,13 @@ from utils import weighted_random_subreddit
 
 # Subreddit that will be a source of content
 subreddit = weighted_random_subreddit({
-    'funny': 1.0,
+    'giveaways': 1.0,
     # If we want get content from several subreddits
     # please provide here 'subreddit': probability
     # 'any_other_subreddit': 0.02
 })
 # Telegram channel with @reddit2telegram_bot as an admin
-t_channel = '@r_funny'
+t_channel = '@giveaway_gift'
 
 
 def send_post(submission, r2t):
