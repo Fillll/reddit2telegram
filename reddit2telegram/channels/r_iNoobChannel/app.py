53fc835ad5c05e936926a9942b16e7e8df65fdbf
alsoGAMER <alsogamer786@gmail.com>
2020-03-02 21:56:24 +0100

    Update app.py

--- reddit2telegram/channels/r_iNoobChannel/app.py
+++ reddit2telegram/channels/r_iNoobChannel/app.py
@@ -6,16 +6,9 @@ t_channel = '@r_iNoobChannel'
 
 def send_post(submission, r2t):
     return r2t.send_simple(submission,
-        # Submission should have at least min_upvotes_limit upvotes.
-        min_upvotes_limit=100,
-        # If you do not want text submissions, just pass False.
         text=True,
-        # If you want gifs, just pass True or text you want under gif.
         gif=True,
-        # If you want images, just pass True or text you want under image.
         img=True,
-        # If you want albums, just pass True or text you want under albums.
         album=True,
-        # If you do not want othe submissions, just pass False.
         other=False
     )
