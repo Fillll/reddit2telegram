d155d9090f32c52b1ecbe7a032824f6a30102a4c
Alexander F <git@fillll.ru>
2020-04-26 19:03:55 +0000

    @awwducational formatting fixed.

--- reddit2telegram/channels/awwducational/app.py
+++ reddit2telegram/channels/awwducational/app.py
@@ -5,4 +5,10 @@ t_channel = '@Awwducational'
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission)
+    return r2t.send_simple(submission,
+    	gif='{title}\n\n{self_text}',
+        img='{title}\n\n{self_text}',
+        album=False,
+        text=False,
+        other=False
+    )
