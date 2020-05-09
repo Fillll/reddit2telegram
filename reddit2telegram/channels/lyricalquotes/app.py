d5e1baaf106d0e74eaa87bb1cc56d6b807334f0e
Alexander F <git@fillll.ru>
2020-05-09 10:28:35 +0000

    New formatting for lyricalquotes.

--- reddit2telegram/channels/lyricalquotes/app.py
+++ reddit2telegram/channels/lyricalquotes/app.py
@@ -5,4 +5,7 @@ t_channel = '@lyricalquotes'
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission, disable_web_page_preview=True)
+    return r2t.send_simple(submission,
+        text='{title}\n\n{short_link}\n{channel}',
+        disable_web_page_preview=True
+    )
