0250c95464e279d5472ee8591c0a55f153d2fd4f
Alexander F <git@fillll.ru>
2020-09-17 19:12:08 +0000

    r_indianmemes only for images.

--- reddit2telegram/channels/r_indianmemes/app.py
+++ reddit2telegram/channels/r_indianmemes/app.py
@@ -5,4 +5,10 @@ t_channel = '@r_indianmemes'
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission)
+    return r2t.send_simple(submission,
+    	text=False,
+        gif=False,
+        img=True,
+        album=False,
+        other=False
+    )
