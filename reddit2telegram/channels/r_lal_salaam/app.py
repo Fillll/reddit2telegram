b8c52c71714c75b22082b54361d909ea1d956ea0
sh3hz <shehax21@gmail.com>
2020-11-16 13:00:24 +0530

    Added r/Lal_Salam

---
+++ reddit2telegram/channels/r_lal_salaam/app.py
@@ -0,0 +1,29 @@
+from utils import weighted_random_subreddit
+
+
+# Subreddit that will be a source of content
+subreddit = weighted_random_subreddit({
+    'lal_salaam': 1.0,
+    # If we want get content from several subreddits
+    # please provide here 'subreddit': probability
+    # 'any_other_subreddit': 0.02
+})
+# Telegram channel with @reddit2telegram_bot as an admin
+t_channel = '@r_lal_salaam'
+
+
+def send_post(submission, r2t):
+    return r2t.send_simple(submission,
+        # If you do not want text submissions, just pass False.
+        text=True,
+        # If you want gifs, just pass True or text you want under gif.
+        gif=True,
+        # If you want videos, just pass True or text you want under gif.
+        video=True,
+        # If you want images, just pass True or text you want under image.
+        img=True,
+        # If you want albums, just pass True or text you want under albums.
+        album=True,
+        # If you do not want othe submissions, just pass False.
+        other=False
+    )
