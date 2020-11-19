ce2c9e033734cb32aa208265940ff3050e9440c6
Alexander F <git@fillll.ru>
2020-11-19 18:23:53 +0000

    Cleaner's log to validate.

--- reddit2telegram/channels/tech_cleaner/app.py
+++ reddit2telegram/channels/tech_cleaner/app.py
@@ -2,16 +2,20 @@ import os
 
 from utils import TEMP_FOLDER
 from utils import SupplyResult
+from utils.tech import get_dev_channel, short_sleep
 
 
 subreddit = 'all'
-t_channel = '@r_channels'
+t_channel = get_dev_channel()
 
 
 def send_post(submission, r2t):
+    total_size = 0
     for filename in os.listdir(TEMP_FOLDER):
         if filename == 'empty.md':
             continue
         file_path = os.path.join(TEMP_FOLDER, filename)
+        total_size += os.path.getsize(file_path)
         os.remove(file_path)
+    r2t.send_text(total_size)
     return SupplyResult.STOP_THIS_SUPPLY
