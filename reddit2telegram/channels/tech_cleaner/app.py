569ef9335d68d57aa766d5aeff60e9bab17b1ed3
Alexander F <git@fillll.ru>
2020-11-20 19:34:21 +0000

    Cleaner with nice logging.

--- reddit2telegram/channels/tech_cleaner/app.py
+++ reddit2telegram/channels/tech_cleaner/app.py
@@ -17,5 +17,5 @@ def send_post(submission, r2t):
         file_path = os.path.join(TEMP_FOLDER, filename)
         total_size += os.path.getsize(file_path)
         os.remove(file_path)
-    r2t.send_text('Deleted: '+ str(total_size) + '.')
+    r2t.send_text('Deleted: ' + str(round(total_size / (1024 * 1024 * 1024.0), 3)) + 'GB.')
     return SupplyResult.STOP_THIS_SUPPLY
