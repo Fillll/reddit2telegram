3a3dbf4b303e8abaf685d8d9771eea2d2966f4f4
Alexander F <git@fillll.ru>
2020-11-21 19:28:49 +0000

    No politics in @reddit_all.

--- reddit2telegram/channels/reddit_all/app.py
+++ reddit2telegram/channels/reddit_all/app.py
@@ -1,10 +1,16 @@
 #encoding:utf-8
 
+from utils import SupplyResult
+
+
 t_channel = '@reddit_all'
 subreddit = 'all'
 
 
 def send_post(submission, r2t):
+    if submission.subreddit.display_name in ['politics']:
+        return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
+
     return r2t.send_simple(submission,
         min_upvotes_limit=12345,
         text='{title}\n\n{self_text}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
