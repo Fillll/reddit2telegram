98537c2372b884309062cbb50ee0cc8e36a73152
Aaron <aaronayalew@gmail.com>
2020-11-26 17:15:54 +0300

    added r_theofficememes

---
+++ reddit2telegram/channels/r_theofficememes/app.py
@@ -0,0 +1,21 @@
+#encoding:utf-8
+
+from utils import weighted_random_subreddit
+
+
+subreddit = weighted_random_subreddit({
+    'theofficememes': 1.0,
+})
+t_channel = '@r_theofficememes
+
+
+def send_post(submission, r2t):
+    return r2t.send_simple(submission,
+        min_upvotes_limit=50,
+        text=False,
+        gif=True,
+        video=True,
+        img=True,
+        album=True,
+        other=False
+    )
