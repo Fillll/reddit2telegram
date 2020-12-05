a512b161894d27e8ec4ef3b31eb08c2b178562f7
Alexander F <git@fillll.ru>
2020-12-05 15:47:45 +0000

    r_girlswithglassessfw only new.

--- reddit2telegram/channels/r_girlswithglassessfw/app.py
+++ reddit2telegram/channels/r_girlswithglassessfw/app.py
@@ -4,5 +4,9 @@ subreddit = 'GirlsWithGlassesSFW'
 t_channel = '@r_GirlsWithGlassesSFW'
 
 
+submissions_ranking = 'new'
+submissions_limit = 1000
+
+
 def send_post(submission, r2t):
     return r2t.send_simple(submission)
