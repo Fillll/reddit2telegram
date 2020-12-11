52487632668bd66cb0f6aba2ad6553530bfdb2d1
Alexander F <git@fillll.ru>
2020-12-11 12:36:04 +0000

    New subs for eminemmemes.

--- reddit2telegram/channels/eminemmemes/app.py
+++ reddit2telegram/channels/eminemmemes/app.py
@@ -1,8 +1,15 @@
 #encoding:utf-8
 
-subreddit = 'EminemMemes'
+subreddit = 'EminemMemes+BadMeetsEvil+D12_+HailieJade'
 t_channel = '@EminemMemes'
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission)
+    return r2t.send_simple(submission,
+        text='{title}\n\n{self_text}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
+        other='{title}\n{link}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
+        album='{title}\n{link}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
+        gif='{title}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
+        img='{title}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}',
+        video='{title}\n\n{upvotes} upvotes\n/r/{subreddit_name}\n{short_link}'
+    )
