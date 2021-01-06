bcdb6e741c822a2edcb2b72c77fce1754d2bf1f2
Alexander F <git@fillll.ru>
2021-01-06 06:16:54 +0000

    Only one flair.

--- reddit2telegram/channels/r_indianmemes/app.py
+++ reddit2telegram/channels/r_indianmemes/app.py
@@ -1,10 +1,17 @@
 #encoding:utf-8
 
+from utils import SupplyResult
+
+
 subreddit = 'IndianMeyMeys+IndianDankMemes+desimemes+TheRawKnee'
 t_channel = '@r_indianmemes'
 
 
 def send_post(submission, r2t):
+	# What to skip
+	if submission.subreddit.display_name == 'TheRawKnee' and submission.link_flair_text != 'Sirf Meme (Non-Rawknee) :D':
+		return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
+
     return r2t.send_simple(submission,
     	text=False,
         gif=False,
