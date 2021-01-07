bfd4329e3a019898d83f78e441e848b979d87f00
Alexander F <git@fillll.ru>
2021-01-07 09:18:15 +0000

    Indention in indianmemes.

--- reddit2telegram/channels/r_indianmemes/app.py
+++ reddit2telegram/channels/r_indianmemes/app.py
@@ -8,12 +8,12 @@ t_channel = '@r_indianmemes'
 
 
 def send_post(submission, r2t):
-	# What to skip
-	if submission.subreddit.display_name == 'TheRawKnee' and submission.link_flair_text != 'Sirf Meme (Non-Rawknee) :D':
-		return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
+    # What to skip
+    if submission.subreddit.display_name == 'TheRawKnee' and submission.link_flair_text != 'Sirf Meme (Non-Rawknee) :D':
+        return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
 
     return r2t.send_simple(submission,
-    	text=False,
+        text=False,
         gif=False,
         img=True,
         album=False,
