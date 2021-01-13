ec4d814c87343ba3e9a417dc45e08dab347dbaeb
Alexander F <git@fillll.ru>
2021-01-13 11:18:21 +0000

    animals_telegram gifs and videos only.

--- reddit2telegram/channels/animals_telegram/app.py
+++ reddit2telegram/channels/animals_telegram/app.py
@@ -5,4 +5,11 @@ t_channel = '@animals_telegram'
 
 
 def send_post(submission, r2t):
-    return r2t.send_simple(submission)
+    return r2t.send_simple(submission,
+        text=False,
+        other=False,
+        album=False,
+        gif=True,
+        img=False,
+        video=True
+    )
