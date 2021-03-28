e1a98dca17b23769361eb8ad8978ae9c5bf60e05
Alexander F <git@fillll.ru>
2021-03-28 06:46:09 +0000

    @metalcunnilingus

---
+++ reddit2telegram/channels/metalcunnilingus/app.py
@@ -0,0 +1,8 @@
+#encoding:utf-8
+
+subreddit = 'BlackMetalMemes+DankMetalMemes+metal_me_irl+MetalMemes+okbuddymetal'
+t_channel = '@MetalCunnilingus'
+
+
+def send_post(submission, r2t):
+    return r2t.send_simple(submission)
