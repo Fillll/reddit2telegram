c5f90214f045717f1698c4cc2fa3bf3b0227f89c
Alexander F <git@fillll.ru>
2021-04-03 06:41:00 +0000

    lyricalquotes text only.

--- reddit2telegram/channels/lyricalquotes/app.py
+++ reddit2telegram/channels/lyricalquotes/app.py
@@ -6,6 +6,6 @@ t_channel = '@lyricalquotes'
 
 def send_post(submission, r2t):
     return r2t.send_simple(submission,
-        text='{title}\n\n{short_link}\n{channel}',
+        text='{title}',
         disable_web_page_preview=True
     )
