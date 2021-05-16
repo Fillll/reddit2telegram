466cea18b80dbf4c6285d2c5ba2e320fbbef6f67
Alexander F <git@fillll.ru>
2021-05-16 08:55:31 +0000

    gatesopencomeonin to wholesomememe

--- reddit2telegram/channels/r_wholesome/app.py
+++ reddit2telegram/channels/r_wholesome/app.py
@@ -1,6 +1,6 @@
 #encoding:utf-8
 
-subreddit = 'wholesome+WholesomeComics+wholesomegifs+wholesomepics+wholesomememes+MadeMeSmile'
+subreddit = 'wholesome+WholesomeComics+wholesomegifs+wholesomepics+wholesomememes+MadeMeSmile+gatesopencomeonin'
 t_channel = '@r_wholesome'
 
 
