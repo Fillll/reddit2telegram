6cc96428be031bc738fd9ee4ec021b00480763e3
Alexander F <git@fillll.ru>
2021-07-29 21:05:04 +0000

    Channels.

--- reddit2telegram/channels/r_greenandpleasant/app.py
+++ reddit2telegram/channels/r_greenandpleasant/app.py
@@ -4,7 +4,7 @@ from utils import weighted_random_subreddit
 
 
 subreddit = weighted_random_subreddit({'greenandpleasant': 1.0,})
-t_channel = 'r_greenandpleasant'
+t_channel = '@r_greenandpleasant'
 
 def send_post(submission, r2t):
     return r2t.send_simple(submission, 
