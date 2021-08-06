6ae2f2793fef6fbce098d86c0604b4d1dd40175a
Alexander F <git@fillll.ru>
2021-08-06 08:26:14 +0000

    Pythondaily finally works well with the code.

--- reddit2telegram/channels/pythondaily/app.py
+++ reddit2telegram/channels/pythondaily/app.py
@@ -21,9 +21,10 @@ def send_post(submission, r2t):
     lwords = info.split(' ')[:200]
     words = ' '.join(lwords)
     return r2t.send_simple(submission,
-        text='{title}\n\n' + words +'\n\n/r/{subreddit_name}\n{short_link}',
+        text='{title}\n\n{words}\n\n/r/{subreddit_name}\n{short_link}',
         gif='{title}\n\n/r/{subreddit_name}\n{short_link}',
         img='{title}\n\n/r/{subreddit_name}\n{short_link}',
         album='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}',
-        other='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}'
+        other='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}',
+        words=words
     )
