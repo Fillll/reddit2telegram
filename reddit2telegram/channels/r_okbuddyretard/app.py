4f45df70e29afefd8cb6bf0d3cc04aec79c820bd
Alexander F <git@fillll.ru>
2021-12-06 20:54:39 +0000

    New channels.Ã

--- reddit2telegram/channels/r_okbuddyretard/app.py
+++
@@ -1,8 +0,0 @@
-#encoding:utf-8
-
-subreddit = 'okboomer+okbuddyretard+AmiITheAsshole+okbuddyretard2'
-t_channel = '@r_okbuddyretard'
-
-
-def send_post(submission, r2t):
-    return r2t.send_simple(submission)
