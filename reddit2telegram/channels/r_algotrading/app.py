d6e396d2b484384fa1f953a51811ce43900b509d
xegim <xegim78479@jasmne.com>
2021-12-15 22:45:19 +0200

    Add algotrading

---
+++ reddit2telegram/channels/r_algotrading/app.py
@@ -0,0 +1,21 @@
+#encoding:utf-8
+
+from utils import weighted_random_subreddit
+
+
+subreddit = weighted_random_subreddit({
+    'algotrading': 1.0,
+})
+t_channel = '@r_algotrading'
+
+
+def send_post(submission, r2t):
+    return r2t.send_simple(submission,
+        min_upvotes_limit=5,
+        text=True,
+        gif=True,
+        video=True,
+        img=True,
+        album=True,
+        other=False
+    )
