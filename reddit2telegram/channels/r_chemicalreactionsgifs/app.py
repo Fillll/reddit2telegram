5d619a568e78c09b66081c6fb200693a6bcb337f
Alexander F <git@fillll.ru>
2021-12-19 09:39:13 +0000

    EXT fix.

--- reddit2telegram/channels/r_chemicalreactionsgifs/app.py
+++ reddit2telegram/channels/r_chemicalreactionsgifs/app.py
@@ -10,13 +10,13 @@ footer = 'by {}'.format(t_channel)
 
 
 def send_post(submission, r2t):
-    what, url, ext = get_url(submission)
+    what, url = get_url(submission)
     title = submission.title
     link = submission.shortlink
     text = '{}\n{}\n\n{}'.format(title, link, footer)
     if what == 'gif':
         if r2t.dup_check_and_mark(url) is True:
             return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
-        return r2t.send_gif_img(what, url, ext, text)
+        return r2t.send_gif_img(what, url, text)
     else:
         return SupplyResult.DO_NOT_WANT_THIS_SUBMISSION
