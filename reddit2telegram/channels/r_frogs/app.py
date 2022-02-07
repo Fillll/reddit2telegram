1402edccb9ca4d73c9fbf73649da219928242267
not-jan <61017633+not-jan@users.noreply.github.com>
2022-02-07 17:59:49 +0100

    Add r_frogs

---
+++ reddit2telegram/channels/r_frogs/app.py
@@ -0,0 +1,31 @@
+#encoding:utf-8
+
+from utils import weighted_random_subreddit
+
+# Subreddit that will be a source of content
+subreddit = weighted_random_subreddit({
+    'frogs': 1.0,
+})
+# Telegram channel with @reddit2telegram_bot as an admin
+t_channel = '@r_frogs'
+
+
+def send_post(submission, r2t):
+    return r2t.send_simple(submission,
+        # Submission should have at least min_upvotes_limit upvotes.
+        min_upvotes_limit=100,
+        # If you do not want text submissions, just pass False.
+        text=False,
+        # If you want gifs, just pass True or text you want under gif.
+        gif=True,
+        # If you want videos, just pass True or text you want under gif.
+        video=True,
+        # If you want images, just pass True or text you want under image.
+        img=True,
+        # If you want Imgur albums, just pass True or text you want under albums.
+        album=False,
+        # If you want Reddit galleries, just pass True or text you want under albums.
+        gallery=False,
+        # If you do not want other submissions, just pass False.
+        other=False
+    )
