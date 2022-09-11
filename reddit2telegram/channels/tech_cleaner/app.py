8d72b00917d9e4e56520fa6036634640bef50504
Fillll <git@alexfil.com>
2022-09-11 14:09:44 +0000

    Free memory notification.

--- reddit2telegram/channels/tech_cleaner/app.py
+++ reddit2telegram/channels/tech_cleaner/app.py
@@ -1,6 +1,8 @@
 from utils import SupplyResult, clean_after_module
 from utils.tech import get_dev_channel
 
+import psutil
+
 
 subreddit = 'all'
 t_channel = get_dev_channel()
@@ -8,5 +10,8 @@ t_channel = get_dev_channel()
 
 def send_post(submission, r2t):
     total_size = clean_after_module()
-    r2t.send_text('Deleted: ' + str(round(total_size / (1024.0 ** 3), 3)) + 'GB.')
+    text_to_send = 'Deleted: ' + str(round(total_size / (1024.0 ** 3), 3)) + 'GB.\n'
+    free_memory_mb = psutil.virtual_memory().free / 1024**2
+    text_to_send += f'Free memory: {free_memory_mb:.3f}MB.'
+    r2t.send_text(text_to_send)
     return SupplyResult.STOP_THIS_SUPPLY
