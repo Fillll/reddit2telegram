8167e7c8a2d71f4d7642c76f0515a32cd5d5c3c1
K0RR <43277609+RDKRACZ@users.noreply.github.com>
2022-11-27 16:17:38 +0100

    Add r/traphentai

---
+++ reddit2telegram/channels/r_traphentai/app.py
@@ -0,0 +1,28 @@
+#encoding:utf-8
+
+from utils import weighted_random_subreddit
+
+
+# Subreddit that will be a source of content
+subreddit = weighted_random_subreddit({
+    'traphentai': 1.0,
+    # If we want get content from several subreddits
+    # please provide here 'subreddit': probability
+    # 'any_other_subreddit': 0.02
+})
+# Telegram channel with @reddit2telegram_bot as an admin
+t_channel = '@r_traphentai'
+
+
+def send_post(submission, r2t):
+    return r2t.send_simple(submission,
+        # Submission should have at least min_upvotes_limit upvotes.
+        min_upvotes_limit=50,
+        text=False,
+        gif=True,
+        video=True,
+        img=True,
+        album=True,
+        gallery=True,
+        other=False
+    )
