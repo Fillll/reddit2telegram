d48a4cd59dedc2d1b37d22c67d615636b428a93a
dealsforindia <pottemasshippo@gmail.com>
2026-01-11 21:42:41 +0530

    Update app.py

--- channels/r_dealsforindia/app.py
+++ channels/r_dealsforindia/app.py
@@ -1,15 +1,21 @@
-#kaiz: 1
-from .model import Subreddit
+#encoding:utf-8
+from utils import weighted_random_subreddit
 
-subreddit = 'dealsforindia'
+subreddit = weighted_random_subreddit({
+    'dealsforindia': 1,
+})
+
 t_channel = '@r_dealsforindia'
 
 def send_post(submission, r2t):
     return r2t.send_simple(submission,
+        check_dups=True,
         min_upvotes_limit=1,
-        text=True,
-        gif=True,
-        img=True,
-        album=True,
-        other=True
+        text='{title}\n\n{self_text}\n\n/r/{subreddit_name}\n{short_link}\n{channel}',
+        other='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}\n{channel}',
+        album='{title}\n{link}\n\n/r/{subreddit_name}\n{short_link}\n{channel}',
+        gif='{title}\n\n/r/{subreddit_name}\n{short_link}\n{channel}',
+        img='{title}\n\n/r/{subreddit_name}\n{short_link}\n{channel}',
+        video='{title}\n\n/r/{subreddit_name}\n{short_link}\n{channel}',
+        gallery='{title}\n\n/r/{subreddit_name}\n{short_link}\n{channel}'
     )
